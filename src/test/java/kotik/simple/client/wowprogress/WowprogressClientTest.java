2f31bedb0c118c02efd91b9a1689109827bca0c2
Noldish <noldish@gmail.com>
2017-03-17 20:39:45 +0400

    no message

--- src/test/java/kotik/simple/client/wowprogress/WowprogressClientTest.java
+++ src/test/java/kotik/simple/client/wowprogress/WowprogressClientTest.java
@@ -1,34 +1,34 @@
-package kotik.simple.client.wowprogress;
-
-import kotik.simple.client.wowprogress.objects.Guild;
-import kotik.simple.configuration.RestClientConfig;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-
-import java.util.List;
-
-import static org.junit.Assert.*;
-
-/**
- * Created by Roman_Kuznetcov on 21.11.2016.
- */
-@ActiveProfiles("Wowprogress-test")
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = RestClientConfig.class, loader = AnnotationConfigContextLoader.class)
-public class WowprogressClientTest {
-
-    @Autowired
-    private WowprogressClient client;
-
-    @Test
-    public void getGuildRanking() throws Exception {
-//        List<Guild> guilds = client.getGuildRanking();
-//        System.out.println(guilds);
-    }
-
-}
+//package kotik.simple.client.wowprogress;
+//
+//import kotik.simple.client.wowprogress.objects.Guild;
+//import kotik.simple.configuration.RestClientConfig;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.support.AnnotationConfigContextLoader;
+//
+//import java.util.List;
+//
+//import static org.junit.Assert.*;
+//
+///**
+// * Created by Roman_Kuznetcov on 21.11.2016.
+// */
+//@ActiveProfiles("Wowprogress-test")
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = RestClientConfig.class, loader = AnnotationConfigContextLoader.class)
+//public class WowprogressClientTest {
+//
+//    @Autowired
+//    private WowprogressClient client;
+//
+//    @Test
+//    public void getGuildRanking() throws Exception {
+////        List<Guild> guilds = client.getGuildRanking();
+////        System.out.println(guilds);
+//    }
+//
+//}
--- src/test/java/kotik/simple/dao/RepositoryManagerTest.java
+++ src/test/java/kotik/simple/dao/RepositoryManagerTest.java
@@ -1,84 +1,84 @@
-package kotik.simple.dao;
-
-import kotik.simple.configuration.DBConfig;
-import kotik.simple.dao.objects.Channel;
-import kotik.simple.dao.objects.CommandDAO;
-import kotik.simple.dao.objects.User;
-import kotik.simple.service.CommandManager;
-import kotik.simple.service.commands.CommandInterface;
-import kotik.simple.service.commands.DeleteCommand;
-import kotik.simple.service.commands.TextCommand;
-import org.junit.*;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-
-import java.util.List;
-import java.util.Map;
-
-/**
- * Created by Roman_Kuznetcov on 16.11.2016.
- */
-@ActiveProfiles("Repository-test")
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = DBConfig.class, loader = AnnotationConfigContextLoader.class)
-@Ignore
-public class RepositoryManagerTest {
-
-    @Autowired
-    RepositoryManager dao;
-
-    @Test
-    public void addUser() throws Exception {
-        dao.addUser(new User("999", "testUser", "displayname", "100"));
-        User user = dao.getUser("999");
-        Assert.assertEquals("testUser",user.getName());
-        dao.deleteUser(user.getId());
-    }
-
-
-    @Test
-    public void addCommand() throws Exception {
-        dao.addCommand(new TextCommand("!testcommand","testcommandtext"));
-        CommandInterface command = dao.getCommand("!testcommand");
-        Assert.assertEquals(command.getDescription(),"testcommandtext");
-        dao.deleteCommand(command.getName());
-    }
-
-
-    @Test
-    public void addChannel() throws Exception {
-        dao.addChannel(new Channel("999","channelName","topictopic"));
-        Channel channel = dao.getChannel("999");
-        Assert.assertEquals("channelName",channel.getName());
-        dao.deleteChannel(channel.getId());
-    }
-
-    @Test
-    public void getAllCommands() throws Exception {
-        Map<String,CommandInterface> commands = dao.getAllCommands();
-        System.out.println(commands);
-    }
-
-    @Test
-    public void getAllUsers() throws Exception {
-
-    }
-
-//    @Test
-//    public void getCommand2(){
+//package kotik.simple.dao;
 //
-//        DeleteCommand command = (DeleteCommand) dao.getCommand("!delete");
-//        System.out.println(command);
+//import kotik.simple.configuration.DBConfig;
+//import kotik.simple.dao.objects.Channel;
+//import kotik.simple.dao.objects.CommandDAO;
+//import kotik.simple.dao.objects.User;
+//import kotik.simple.service.CommandManager;
+//import kotik.simple.service.commands.CommandInterface;
+//import kotik.simple.service.commands.DeleteCommand;
+//import kotik.simple.service.commands.TextCommand;
+//import org.junit.*;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.support.AnnotationConfigContextLoader;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+///**
+// * Created by Roman_Kuznetcov on 16.11.2016.
+// */
+//@ActiveProfiles("Repository-test")
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = DBConfig.class, loader = AnnotationConfigContextLoader.class)
+//@Ignore
+//public class RepositoryManagerTest {
+//
+//    @Autowired
+//    RepositoryManager dao;
+//
+//    @Test
+//    public void addUser() throws Exception {
+//        dao.addUser(new User("999", "testUser", "displayname", "100"));
+//        User user = dao.getUser("999");
+//        Assert.assertEquals("testUser",user.getName());
+//        dao.deleteUser(user.getId());
 //    }
-
-
-
-    @AfterClass
-    public static void doYourOneTimeTeardown() {
-
-    }
-}
+//
+//
+//    @Test
+//    public void addCommand() throws Exception {
+//        dao.addCommand(new TextCommand("!testcommand","testcommandtext"));
+//        CommandInterface command = dao.getCommand("!testcommand");
+//        Assert.assertEquals(command.getDescription(),"testcommandtext");
+//        dao.deleteCommand(command.getName());
+//    }
+//
+//
+//    @Test
+//    public void addChannel() throws Exception {
+//        dao.addChannel(new Channel("999","channelName","topictopic"));
+//        Channel channel = dao.getChannel("999");
+//        Assert.assertEquals("channelName",channel.getName());
+//        dao.deleteChannel(channel.getId());
+//    }
+//
+//    @Test
+//    public void getAllCommands() throws Exception {
+//        Map<String,CommandInterface> commands = dao.getAllCommands();
+//        System.out.println(commands);
+//    }
+//
+//    @Test
+//    public void getAllUsers() throws Exception {
+//
+//    }
+//
+////    @Test
+////    public void getCommand2(){
+////
+////        DeleteCommand command = (DeleteCommand) dao.getCommand("!delete");
+////        System.out.println(command);
+////    }
+//
+//
+//
+//    @AfterClass
+//    public static void doYourOneTimeTeardown() {
+//
+//    }
+//}
